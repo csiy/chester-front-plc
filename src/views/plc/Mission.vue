9539ac7f4be5420d0f39dbc8edd865bcd9f171f4
chest <chester_csy@outlook.com>
2021-02-22 16:31:15 +0800

    最后的修改

--- src/views/plc/Mission.vue
+++ src/views/plc/Mission.vue
@@ -74,10 +74,10 @@
           <!--                        <v-icon left>mdi-upload</v-icon>-->
           <!--                        导入-->
           <!--                    </v-btn>-->
-          <v-btn @click="plusItem" color="orange darken-2" small>
-            <v-icon left>mdi-plus</v-icon>
-            添加
-          </v-btn>
+<!--          <v-btn @click="plusItem" color="orange darken-2" small>-->
+<!--            <v-icon left>mdi-plus</v-icon>-->
+<!--            添加-->
+<!--          </v-btn>-->
         </v-row>
       </template>
       <template v-slot:item.updatedOn="{ item }">
